3c0cd7abd8286d6f7039e7564b0061d3332fa2b8
borbai <114570888+borbai@users.noreply.github.com>
2023-05-14 01:30:05 -0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const RESOURCES = {
   "assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
-"assets/NOTICES": "24b8f722ee26077ae8cd631c0e3b20b3",
+"assets/NOTICES": "249ce9bd70ed56e2a9f253dd1084d820",
 "canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
 "canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e",
 "canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
@@ -17,9 +17,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "6a0a7b17b6df20114a15958937472193",
-"/": "6a0a7b17b6df20114a15958937472193",
-"main.dart.js": "1512422d7076ef71e23a568d8e68c3af",
+"index.html": "72561a82c5309fed61582381414aa7fd",
+"/": "72561a82c5309fed61582381414aa7fd",
+"main.dart.js": "3e7b7a900ae53c4e9065ff28945a16af",
 "manifest.json": "ad21dc64c63cd61b2ac3a94b97289074",
 "version.json": "04c96d538d7f2abefff9c192a010e0ae"
 };
